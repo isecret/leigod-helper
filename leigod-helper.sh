260f138367321072b10419fe64d8229fb06939c2
Secret <i@wangmao.me>
2023-11-14 14:24:54 +0800

    init repo

---
+++ leigod-helper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# 手机号
+USERNAME="18866668888"
+# 密码
+PASSWORD="password"
+
+if ! command -v "jq" > /dev/null; then
+    echo "缺失 jq 依赖，请执行命令手动安装";
+    echo "Ubuntu: sudo apt-get install -y jq"
+    echo "CentOS: sudo yum install -y jq"
+    echo "MacOS sudo brew install -y jq"
+    exit;
+fi
+
+password_hash=$(echo -n "$PASSWORD" | md5)
+login=$(curl -Lks -X POST https://webapi.leigod.com/api/auth/login \
+    -H 'content-type: application/json' \
+    -d "{
+        \"account_token\": null,
+        \"country_code\": 86,
+        \"lang\": \"zh_CN\",
+        \"password\": \"$password_hash\",
+        \"region_code\": 1,
+        \"src_channel\": \"guanwang\",
+        \"user_type\": \"0\",
+        \"username\": \"$USERNAME\"
+    }")
+login_code=$(echo $login | jq -r ".code" 2>&1)
+login_msg=$(echo $login | jq -r ".msg" 2>&1)
+if [ "$login_code" -eq 0 ]; then
+    account_token=$(echo $login | jq -r ".data.login_info.account_token" 2>&1)
+    pause=$(curl -Lks -X POST https://webapi.leigod.com/api/user/pause \
+        -H 'content-type: application/json' \
+        -d "{
+                \"account_token\": \"$account_token\",
+                \"lang\": \"zh_CN\"
+            }")
+    pause_code=$(echo $pause | jq -r ".code" 2>&1)
+    pause_msg=$(echo $pause | jq -r ".msg" 2>&1)
+
+    echo "暂停结果: $pause_msg"
+else
+    echo "登录失败...原因: $login_msg"
+fi
---
+++ readme.md
@@ -0,0 +1,52 @@
+## ⚡️雷神加速器暂停助手
+
+⚡️基于雷神加速器 Web 接口实现自动登录和暂停计时
+
+## 使用方法
+
+部署本地或服务器中，需先安装依赖；
+
+```bash
+# Ubuntu/Debain
+$ apt-get install -y jq
+
+# CentOS
+$ yum install -y jq
+
+# MacOS
+$ brew install -y jq
+```
+
+克隆仓库或下载代码包并解压；
+
+```bash
+git clone --depth=1 https://github.com/isecret/leigod-helper.git
+# 因为国内网络克隆速度不是很理想的，选择下面的
+git clone --depth=1 https://ghproxy.com/isecret/leigod-helper.git
+```
+
+修改配置，编辑 `leigod-helper.sh` 修改用户名和密码；
+
+```bash
+# 手机号
+USERNAME="18866668888"
+# 密码
+PASSWORD="password"
+```
+
+保存编辑，添加脚本可执行权限并尝试运行；
+
+```bash
+$ sudo chmod +x leigod-helper.sh && ./leigod-helper.sh
+暂停结果: ...
+```
+
+添加定时任务定时自动执行；
+
+```bash
+# 每天凌晨 2:00 暂停计时
+0 2 * * * bash /path/to/leigod-helper.sh >> /path/to/leigod-helper.log 2>&1
+```
+
+## Todo
+- 任务通知
